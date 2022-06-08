5893f3269be988b0be6cc5af68b68d0e4a0a567f
CarlosIsidorVictoria <cvictoria@student.ie.edu>
2022-06-08 19:03:21 +0000

    Declare black as development dependency

--- src/ie_titanic_utils/str_utils.py
+++ src/ie_titanic_utils/str_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-def extract_title(name, separator = "."):
+
+def extract_title(name, separator="."):
     if not separator:
         raise ValueError("Separator cannot be empty")
     fragments = name.split(separator)
